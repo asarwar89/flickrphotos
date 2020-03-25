eb8c673006560132439abb3e672c6a79b3800b87
Sarwar Ahmed <asarwar89@yahoo.com>
2020-03-25 15:03:16 +1100

    File update

--- src/components/FlickrPhotos/FlickrPhotos.js
+++ src/components/FlickrPhotos/FlickrPhotos.js
@@ -6,29 +6,49 @@ import classes from './FlickrPhotos.module.css';
 
 class FlickrPhotos extends Component {
 
-  state = {
-    loadedSearchResult: null,
-    propSearchString: null,
-    isResult: null
+  // state = {
+  //   loadedSearchResult: null,
+  //   propSearchString: null,
+  //   isResult: null
+  // }
+
+  constructor(){
+    super();
+    this.state = {
+      loadedSearchResult: null,
+      propSearchString: null,
+      isResult: null
+    }
+    this.timeout = null;
   }
 
   //Front-end apps api request is blocked by CORS policy in browsers
   //So used cors-anywhere app to get "Access-Control-Allow-Origin: *"
   componentDidUpdate () {
-    if (this.props.searchString !== '') {
-      if (!this.state.loadedSearchResult || 
-        (this.state.loadedSearchResult && this.state.propSearchString !== this.props.searchString)) {
-        const propLocalSearchString = this.props.searchString;
-          axios.get('https://cors-anywhere.herokuapp.com/https://www.flickr.com/services/feeds/photos_public.gne?tags=' + this.props.searchString.split(' ').join(',') + '&format=json&nojsoncallback=1')
-              .then(response => {
-                  this.setState({loadedSearchResult: response.data.items, propSearchString:propLocalSearchString});
-              });
-      }
-    } else {
-      if (this.state.propSearchString !== this.props.searchString) {
-        this.setState({loadedSearchResult: null, propSearchString:''});
-      }
+
+    if (this.timeout !== null) {
+      clearTimeout(this.timeout);
     }
+
+    this.timeout = setTimeout(() => {
+
+      if (this.props.searchString !== '') {
+        if (!this.state.loadedSearchResult || 
+          (this.state.loadedSearchResult && this.state.propSearchString !== this.props.searchString)) {
+          const propLocalSearchString = this.props.searchString;
+            axios.get('https://cors-anywhere.herokuapp.com/https://www.flickr.com/services/feeds/photos_public.gne?tags=' + this.props.searchString.split(' ').join(',') + '&format=json&nojsoncallback=1')
+                .then(response => {
+                    this.setState({loadedSearchResult: response.data.items, propSearchString:propLocalSearchString});
+                });
+        }
+      } else {
+        if (this.state.propSearchString !== this.props.searchString) {
+          this.setState({loadedSearchResult: null, propSearchString:''});
+        }
+      }
+
+    },1000)
+
   }
 
   render() {
